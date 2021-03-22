ff9621195a59e92e06734b3b142f19e74a199224
ignamadevollen@gmail.com <ignamadevollen@gmail.com>
2021-03-22 11:25:05 -0300

    query.py: handle queries implemented with pandas

--- myproject/movies_app/queries.py
+++ myproject/movies_app/queries.py
@@ -1,42 +1,30 @@
 import os
-import csv
+import pandas as pd
 
 
 # Dictionary of pairs (currency, coefficient), used to convert the different 
 # currencies in the USA budget data to dollars. Example: EUR 5 => $ (1.19 * 5)
-dict_currency = {'GBP': 1.39, 'CAD': 0.79, 'PYG': 0.00015, 'ESP': 1.19,
- 'AUD': 0.77, 'EUR': 1.19, 'RUR': 0.014}
+DICT_CURRENCY = {'GBP': 1.39, 'CAD': 0.79, 'PYG': 0.00015, 'ESP': 1.19,
+ 'AUD': 0.77, 'EUR': 1.19, 'RUR': 0.014, '$': 1}
   
-
 # Convert [currency] amount to [dollar] amount
 def convert_amount_to_dollar(currency, amount):
-    if currency in dict_currency.keys():
-        return amount*dict_currency[currency]
-    # Error: currency is not in dict_currency, just return amount
+    if currency in DICT_CURRENCY.keys():
+        return amount*DICT_CURRENCY[currency]
+    # Error: currency is not in DICT_CURRENCY, just return amount
     else:
         return amount
 
-# Return the queries of exercise 2. 
+def convert_budget_to_amount(budget):
+    amount = budget.split(' ')
+    return convert_amount_to_dollar(amount[0], int(amount[1]))
+
 def handle_queries():
-    """
-        Return (avg_cost, total_cost), where:
-            avg_cost is: costo promedio por pelicula de todas las peliculas cuyo pais es USA
-            total_cost is: costo total (budget) de todas las peliculas cuyo pais es USA 
-    """
-    count_movies_USA, total_cost = 0, 0
     dirname = os.path.split(os.path.abspath(__file__))[0] + '/files/movies.csv'
-    with open(dirname, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            if row['country'] == 'USA':
-                count_movies_USA += 1
-                if row['budget']:
-                    budget = row['budget'].split(' ')
-                    if budget[0] != '$':
-                        total_cost += convert_amount_to_dollar(budget[0], int(budget[1]))
-                    else:
-                        total_cost += int(budget[1])
-
-    avg_cost = total_cost/count_movies_USA
-
+    df = pd.read_csv(dirname, low_memory=False)
+    df = df[df['country'] == 'USA']['budget']
+    df = df.fillna('$ 0')
+    count_movies = df.count()
+    total_cost = df.apply(convert_budget_to_amount).sum()
+    avg_cost = total_cost/count_movies
     return (int(avg_cost), int(total_cost))
